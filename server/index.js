8ed92b7b181020c4b1e6ff3342a867f2d0a4cd87
Kultup <gorodok048@gmail.com>
2026-03-23 15:34:16 +0200

    22112

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import db from './db.js';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
