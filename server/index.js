6533e9c9c46ac5e69cb3d988e3146f74fc445e71
Kultup <gorodok048@gmail.com>
2026-03-23 15:42:16 +0200

    Add debug logging and finalize router fixes

--- server/index.js
+++ server/index.js
@@ -79,6 +79,12 @@ app.use(cors({
 app.use(express.json({ limit: '10kb' })); // Limit body size
 app.use(rateLimit);
 
+// Logger to debug requests on the server
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
+
 // Get all categories
 app.get('/api/categories', (req, res) => {
   db.all("SELECT * FROM categories", [], (err, rows) => {
